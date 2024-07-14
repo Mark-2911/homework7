d44fd0173b472ac6a4f28c8f191fc702c956b0d6
HP1 <mmechanov@gmail.com>
2024-07-14 16:11:15 +0300

    corrected

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,10 @@ public class Main {
         int total = 0;
         int saving = 15000;
         int month = 0;
-        while (total <= 2_459_000) {
-            total = total + saving;
-            month = month + 1;
+        int amountOfAccumulation = 2_459_000;
+        while (total <= amountOfAccumulation) {
+            total += saving;
+            month += 1;
             System.out.println("Месяц " + month + " сумма накопления " + total + " рублей");
 
         }
@@ -31,9 +32,12 @@ public class Main {
         System.out.println("Задача 3");
 
         int population = 12_000_000;
+        int death = 8;
+        int life = 17;
+        int perPerson = 1000;
         for (int i = 1; i <= 10; i = i + 1) {
-            int birthRate = 17 * population / 1000;
-            int mortalityRate = 8 * population / 1000;
+            int birthRate = life * population/perPerson;
+            int mortalityRate = death * population/perPerson;
             population = population + birthRate - mortalityRate;
             System.out.println("Год " + i + " численность населения состовляет " + population + " человек");
 
@@ -41,41 +45,46 @@ public class Main {
 
         System.out.println("Задача 4");
 
-        int total1 = 0;
-        int saving1 = 15000;
-        int month1 = 0;
-        while (total1 <= 12_000_000) {
-            total1 = total1 + saving1 + (total1 * 7)/100;
-            month1 = month1 + 1;
-            System.out.println("Месяц " + month1 + " сумма накопления " + total1 + " рублей");
+        total = 0;
+        saving = 15000;
+        month = 0;
+        int percentage = (saving * 7)/100;
+        int depositWithInerest = saving + percentage;
+        amountOfAccumulation = 12_000_000;
+        while (total <= amountOfAccumulation) {
+            total += depositWithInerest;
+            month += 1;
+            System.out.println("Месяц " + month + " сумма накопления " + total + " рублей");
 
         }
 
         System.out.println("Задача 5");
 
-        int total2 = 0;
-        int saving2 = 15000;
-        int month2 = 0;
-        while (total2 <= 12_000_000) {
-            total2 = total2 + saving2 + (total2 * 7)/100;
-            month2 = month2 + 1;
-            if (month2 % 6 == 0)
-             System.out.println("Месяц " + month2 + " сумма накопления " + total2 + " рублей");
+        total = 0;
+        saving = 15000;
+        month = 0;
+        while (total <= amountOfAccumulation) {
+            total += depositWithInerest;
+            month += 1;
+            if (month % 6 == 0) {
+                System.out.println("Месяц " + month + " сумма накопления " + total + " рублей");
+            }
 
         }
 
         System.out.println("Задача 6");
 
-        int saving3 = 15000;
-        int total3 = 0;
+        saving = 15000;
+        total = 0;
         int year = 9;
         int monthYear = 12;
         int totalMonth = year * monthYear;
         for (int month3 = 1; month3 <= totalMonth; month3++){
-            total3 = total3 + (total3 * 7)/100;
-            total3 = saving3 + total3;
-            if(month3 % 6 == 0)
-             System.out.println("Месяц " + month3 + " сумма накопления " + total3);
+            total += depositWithInerest;
+            total += saving;
+            if(month3 % 6 == 0) {
+                System.out.println("Месяц " + month3 + " сумма накопления " + total);
+            }
 
         }
 
@@ -83,7 +92,8 @@ public class Main {
         System.out.println("Задача 7");
 
         int friday = 5;
-        while (friday <= 31) {
+        int daysInAMonth = 31;
+        while (friday <= daysInAMonth) {
             System.out.println("Сегодня пятница, " + friday + "-е число.Необходимо подготовить отчет");
             friday += 7;
         }
@@ -91,11 +101,13 @@ public class Main {
         System.out.println("Задача 8");
 
 
-        int yaerBefore = 2024 - 200;
+        int yearBefore = 2024 - 200;
         int yearAfter = 2024 + 100;
-        for (year = yaerBefore; year < yearAfter; year++) {
-            if (year % 79 == 0)
+        int cametsArrivalInterval = 79;
+        for (year = yearBefore; year < yearAfter; year++) {
+            if (year % cametsArrivalInterval == 0) {
                 System.out.println(year);
+            }
 
         }
 
